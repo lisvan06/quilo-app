5607d7cdbaf65f1e19a0d416c303926612ef45ca
lisvan06 <lisvan06@gmail.com>
2023-08-09 18:41:24 -0600

    Deleting a nav component for test request in the page

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -1,28 +1,7 @@
 export default function HelpPage() {
     return (
     <div>
-      <nav className="flex justify-end bg-blue-500 py-4 px-8">
-      <ul className="flex space-x-4 items-center">
-        <li className="text-white hover:shadow-md">
-          <a href="#" className="hover:text-blue-300">Home</a>
-        </li>
-        <li className="text-white hover:shadow-md">
-          <a href="#" className="hover:text-blue-300">About</a>
-        </li>
-        <li className="text-white hover:shadow-md">
-          <a href="#" className="hover:text-blue-300">Profile</a>
-        </li>
-        <li className="text-white hover:shadow-md">
-          <a href="#" className="hover:text-blue-300">Login</a>
-        </li>
-        <li className="text-white hover:shadow-md">
-          <a href="#" className="hover:text-blue-300">Logout</a>
-        </li>
-        <li className="text-white hover:shadow-md">
-          <a href="#" className="hover:text-blue-300">Register</a>
-        </li>
-      </ul>
-    </nav>
+      <h1>This is a Help Page</h1>
     </div>
   )
 
