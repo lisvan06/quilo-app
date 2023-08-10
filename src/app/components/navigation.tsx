12365763d221b4932035636d0be635cbfd1f803e
lisvan06 <lisvan06@gmail.com>
2023-08-09 18:57:41 -0600

    Fixing route error in dasboard by dashboard

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -157,7 +157,7 @@ export default function NavigationBar() {
                           <Menu.Item>
                             {({ active }) => (
                               <a
-                                href="/dasboard/products"
+                                href="/dashboard/products"
                                 className={classNames(
                                   active ? "bg-gray-100" : "",
                                   "block px-4 py-2 text-sm text-gray-700"
