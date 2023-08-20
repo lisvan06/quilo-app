c0943477c05c0bbae913371c84dd9d048b442043
lisvan06 <lisvan06@gmail.com>
2023-08-19 19:26:09 -0600

    The products page dont show any product in vercel

--- src/app/dashboard/products/page.jsx
+++ src/app/dashboard/products/page.jsx
@@ -1,25 +1,32 @@
 import TableProducts from "@/app/components/client/tableProducts";
 import { getServerSession } from "next-auth";
-
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-export default async function productsPage() {  
-async function getData(user) {
-  try {
-    const role = user.role;
-    // role = "ADMIN";
-    const res =  (role === "USER") ? await fetch(`${process.env.BASE_URL}/api/product/search?ownerId=${user.id}`, {
-      cache: "no-store"
-    }) : await fetch(`${process.env.BASE_URL}/api/product`, {
-      cache: "no-store"
-    });
-    const data = await res.json();
-
-    return data;
-  } catch (error) {}
-}
-
+export default async function productsPage() {
   const session = await getServerSession(authOptions);
+
+  async function getData(user) {
+    try {
+      console.log(process.env.BASE_URL + "/api/product/search");
+      const role = user.role;
+      // role = "ADMIN";
+      const res =
+        role === "USER"
+          ? await fetch(
+              `${process.env.BASE_URL}/api/product/search?ownerId=${user.id}`,
+              {
+                cache: "no-store",
+              }
+            )
+          : await fetch(`${process.env.BASE_URL}/api/product`, {
+              cache: "no-store",
+            });
+      const data = await res.json();
+
+      return data;
+    } catch (error) {}
+  }
+
   try {
     const data = await getData(session.user);
     return (
