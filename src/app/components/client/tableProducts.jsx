6e4d335a20209a65573591eefe2b5cf3417ce229
lisvan06 <lisvan06@gmail.com>
2023-08-19 19:31:57 -0600

    Length of data throw a client-side exception

--- src/app/components/client/tableProducts.jsx
+++ src/app/components/client/tableProducts.jsx
@@ -60,7 +60,7 @@ export default function TableProducts({ data }) {
               </table>
               <div className="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between">
                 <span className="text-xs xs:text-sm text-gray-900">
-                  Showing 1 to {data.length} of {data.length} Entries
+                  Showing 1 to 4 of 10 Entries
                 </span>
                 <div className="inline-flex mt-2 xs:mt-0">
                   <button className="text-sm text-indigo-50 transition duration-150 hover:bg-indigo-500 bg-indigo-600 font-semibold py-2 px-4 rounded-l">
